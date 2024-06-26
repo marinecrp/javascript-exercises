b2b8fb537e8ce0a1e6189b29119da23ff845f466
Marine Carpe <marinecarpe@MacBook-Pro-de-Marine.local>
2024-06-26 15:55:56 -0400

    Javascript Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    let removedItemsArray = [];
+    for (let i = 1; i < arguments.length; i++){
+        removedItemsArray.push(arguments[i]);
+    }
+    let newArray = [];
+    for (const item of array){
+        if (!removedItemsArray.includes(item)){
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
