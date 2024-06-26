692ce9a8c6dade62c9456aaf478502783f81c176
Marine Carpe <marinecarpe@MacBook-Pro-de-Marine.local>
2024-06-26 16:11:18 -0400

    Javascript Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0 || typeof(firstNumber) !== "number" || typeof(secondNumber) !== "number" ) {
+        return 'ERROR';
+    }
+    let smallNumber = firstNumber <= secondNumber ? firstNumber : secondNumber;
+    let bigNumber = firstNumber >= secondNumber ? firstNumber : secondNumber;
+    let sum = 0;
+    for(let i = smallNumber; i<= bigNumber ; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
